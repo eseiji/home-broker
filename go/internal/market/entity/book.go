c93d358d2e4a22ed3064d8211b87b3ef0e319dcc
eseiji <enzoseiji14@gmail.com>
2025-04-09 15:57:44 -0300

    finish microsservice implementation

--- go/internal/market/entity/book.go
+++ go/internal/market/entity/book.go
@@ -29,3 +29,99 @@ func (oq *orderQueue) GetNextOrder() *Order {
 	*oq = (*oq)[1:]
 	return order
 }
+
+func (b *Book) Trade() {
+	buyOrders := make(map[string]*orderQueue)
+	sellOrders := make(map[string]*orderQueue)
+
+	for order := range b.IncomingOrders {
+		assetId := order.Asset.ID
+
+		if buyOrders[assetId] == nil {
+			buyOrders[assetId] = &orderQueue{}
+		}
+
+		if sellOrders[assetId] == nil {
+			sellOrders[assetId] = &orderQueue{}
+		}
+
+		if order.OrderType == "BUY" {
+			b.tryMatch(order, sellOrders[assetId], buyOrders[assetId])
+		} else {
+			b.tryMatch(order, buyOrders[assetId], sellOrders[assetId])
+		}
+	}
+}
+
+func (b *Book) tryMatch(newOrder *Order, availableOrders, pendingOrders *orderQueue) {
+	for {
+		potentialMatch := availableOrders.GetNextOrder()
+
+		if potentialMatch == nil {
+			break
+		}
+
+		if !b.pricesMatch(newOrder, potentialMatch) {
+			availableOrders.Add((potentialMatch))
+			break
+		}
+
+		if potentialMatch.PendingShares > 0 {
+			matchedTransaction := b.createTransaction(newOrder, potentialMatch)
+			b.processTransaction(matchedTransaction)
+
+			if potentialMatch.PendingShares > 0 {
+				availableOrders.Add(potentialMatch)
+			}
+
+			if newOrder.PendingShares == 0 {
+				break
+			}
+		}
+	}
+
+	if newOrder.PendingShares > 0 {
+		pendingOrders.Add(newOrder)
+	}
+}
+
+func (b *Book) pricesMatch(order, matchOrder *Order) bool {
+	if order.OrderType == "BUY" {
+		return matchOrder.Price <= order.Price
+	}
+
+	return matchOrder.Price >= order.Price
+}
+
+func (b *Book) createTransaction(incomingOrder, matchedOrder *Order) *Transaction {
+	var buyOrder, sellOrder *Order
+
+	if incomingOrder.OrderType == "BUY" {
+		buyOrder, sellOrder = incomingOrder, matchedOrder
+	} else {
+		buyOrder, sellOrder = matchedOrder, incomingOrder
+	}
+
+	shares := incomingOrder.PendingShares
+
+	if matchedOrder.PendingShares < shares {
+		shares = matchedOrder.PendingShares
+	}
+
+	return NewTransaction(sellOrder, buyOrder, shares, matchedOrder.Price)
+}
+
+func (b *Book) recordTransaction(transaction *Transaction) {
+	b.Transactions = append(b.Transactions, transaction)
+	transaction.BuyOrder.Transactions = append(transaction.BuyOrder.Transactions, transaction)
+	transaction.SellOrder.Transactions = append(transaction.SellOrder.Transactions, transaction)
+}
+
+func (b *Book) processTransaction(transaction *Transaction) {
+	defer b.Wg.Done()
+
+	transaction.Process()
+	b.recordTransaction(transaction)
+	b.ProcessedOrders <- transaction.BuyOrder
+	b.ProcessedOrders <- transaction.SellOrder
+}
